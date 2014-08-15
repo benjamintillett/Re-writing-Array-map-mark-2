7aed94a1f4a5d86dadd36a5e382b1b6b4a40aaec
benjamintillett <benjamin.tillett@gmail.com>
2014-08-15 19:25:41 +0100

    create the king_and_his_rocks.rb and non recursive implementation

--- spec/my_each_spec.rb
+++ spec/my_each_spec.rb
@@ -2,8 +2,14 @@ require 'my_each'
 describe Array do 
 	context "An array of Fixnums [1,2,3,4,5]" do
 	let(:array_of_fixnums) { [1,2,3,4,5] }	
-		it "should return itself when each called" do 
-			expect(array_of_fixnums.my_each{}).to be array_of_fixnums  
-		end
-	end
+		it "should pass each element of an array to a block one when each called" do 
+# 			expect(array_of_fixnums.my_each{}).to be array_of_fixnums  
+# 		end
+# 	end
+	
+# end
+arr = [1,2,3,4]
+arr.my_each.with_index(5) do |el, index|
+	expect(el).to eq arr[index]
 end
+
---
+++ the_king_and_his_rocks.rb
@@ -0,0 +1,14 @@
+# make some random rocks
+rocks = 30.times.map{rand(200) + 1}
+puts rocks.join(', ')
+max_rock = 0 
+
+rocks.each do |rock|
+	max_rock = rock if max_rock < rock
+end
+
+puts "Heaviest rock is: #{max_rock}"
+
+# or with inject 
+
+puts "Heaviest rock is: #{rocks.inject { |max_rock, rock| rock > max_rock ? rock : max_rock }}"
