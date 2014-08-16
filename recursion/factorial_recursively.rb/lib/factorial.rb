858db5f344537b395b1f3a68a5e74cd96af1a447
benjamintillett <benjamin.tillett@gmail.com>
2014-08-16 06:50:33 +0100

    add working recursive factorial method

--- recursion/factorial_recursively.rb/lib/factorial.rb
+++ recursion/factorial_recursively.rb/lib/factorial.rb
@@ -1,3 +1,12 @@
+# def factorial(number)
+# 	(1..number).inject(1) { |sum,i| sum * i }
+# end
+
+
 def factorial(number)
-	(1..number).inject(1) { |sum,i| sum * i }
+	if number == 0 
+	   1 
+	else 
+		number * factorial(number - 1)
+	end
 end
