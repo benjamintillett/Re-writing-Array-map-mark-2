d0081ba4f495b671497e351bbcf9c4dba8ce5883
benjamintillett <benjamin.tillett@gmail.com>
2014-08-17 19:45:12 +0100

    each add spec: should pass each element of an array one at a time to a block when each called

--- spec/my_each_spec.rb
+++ spec/my_each_spec.rb
@@ -1,15 +1,13 @@
-# require 'my_each'
-# describe Array do 
-# 	context "An array of Fixnums [1,2,3,4,5]" do
-# 	let(:array_of_fixnums) { [1,2,3,4,5] }	
-# 		it "should pass each element of an array to a block one when each called" do 
-# # 			expect(array_of_fixnums.my_each{}).to be array_of_fixnums  
-# # 		end
-# # 	end
-	
-# # end
-# arr = [1,2,3,4]
-# arr.my_each.with_index(5) do |el, index|
-# 	expect(el).to eq arr[index]
-# end
+require 'my_each'
+describe Array do 
+	context "An array of Fixnums [1,2,3,4,5]" do
+	let(:array_of_fixnums) { [1,2,3,4,5] }	
+		it "should pass each element of an array one at a time to a block when each called" do 
+			array_of_fixnums.each.with_index(0) do |el, index|
+				expect(el).to eq array_of_fixnums[index]
+			end
+		end
+	end
+end
+
 
