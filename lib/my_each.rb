d7dd0c8852329878bcc4ca7f713737905217eb1d
benjamintillett <benjamin.tillett@gmail.com>
2014-08-17 19:55:14 +0100

    add working implementation of my_each.rb

--- lib/my_each.rb
+++ lib/my_each.rb
@@ -1,6 +1,8 @@
 class Array 
-	def my_each 
-		self
+	def my_each(&block) 
+		return to_enum(:recursive_map) unless block_given?
+		for i in 0..self.length-1
+			block.call(self[i])
+		end
 	end
 end
-
--- spec/my_each_spec.rb
+++ spec/my_each_spec.rb
@@ -3,7 +3,8 @@ describe Array do
 	context "An array of Fixnums [1,2,3,4,5]" do
 	let(:array_of_fixnums) { [1,2,3,4,5] }	
 		it "should pass each element of an array one at a time to a block when each called" do 
-			array_of_fixnums.each.with_index(0) do |el, index|
+			array_of_fixnums.my_each.with_index(0) do |el, index|
+				puts el
 				expect(el).to eq array_of_fixnums[index]
 			end
 		end
